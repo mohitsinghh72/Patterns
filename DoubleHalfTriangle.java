2234a10fd4263912903707aac97244b2e3b027bc
Mohit Singh <mohitsinghh72@gmail.com>
2025-11-19 16:09:42 +0530

    Half Diamond like Pattern

---
+++ DoubleHalfTriangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DoubleHalfTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        //Increasing part
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //decreasing part
+        for(int i = 0;i<n;i++){
+            for(int j = n-1-i;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
