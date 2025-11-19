62e476c7fecd12f7e7416a136f801df51692c7a9
Mohit Singh <mohitsinghh72@gmail.com>
2025-11-19 18:30:04 +0530

    Left-aligned triangle Pattern

--- HalfTriangle.java
+++ HalfTriangle.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
-public class HalfTriangle {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows you want in Triangle : ");
         int n = sc.nextInt();
 
         for(int i = 0;i<n;i++){
