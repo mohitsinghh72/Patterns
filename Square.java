528f66a352eca902b1424a9314b7b9ed69207120
Mohit Singh <mohitsinghh72@gmail.com>
2025-11-19 18:24:43 +0530

    Square shape Pattern

--- Square.java
+++ Square.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Square {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int r = sc.nextInt();                   //type the rows you want in square
-        int c = sc.nextInt();                   //type the columns you want in square
+        System.out.print("Enter the number of rows you want in Square : ");
+        int r = sc.nextInt();
 
         for(int i = 0;i<r;i++){
-            for(int j = 0;j<c;j++){
+            for(int j = 0;j<r;j++){
                 System.out.print("*"+" ");
             }
             System.out.println();
