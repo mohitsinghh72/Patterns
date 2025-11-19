981add2e4bebe15d693e2ef415d845deb4554e26
Mohit Singh <mohitsinghh72@gmail.com>
2025-11-19 18:52:03 +0530

    Centered Pyramid Pattern

---
+++ Pyramid.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the row number in pyramid : ");
+        int n = sc.nextInt();
+        for(int i = 0;i<n;i++){
+            for(int j = 0; j<n-1-i;j++){
+                System.out.print(" ");
+            }
+            for(int k = 0;k<2*i+1;k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
